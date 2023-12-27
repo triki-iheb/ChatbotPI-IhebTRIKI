e87ba6773f09eb63a8a71bfd99b67bb1e2cd1ba4
TRIKIIHEB <i.triki21408@pi.tn>
2023-12-27 10:07:17 +0100

    V_Final

--- ChatbotPI_IHEB/BACKEND/main.py
+++ ChatbotPI_IHEB/BACKEND/main.py
@@ -1,8 +1,16 @@
-from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
+import openai
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 import uvicorn
 import nltk
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+import string
+
+app = FastAPI()
+
+
 app = FastAPI()
 
 app.add_middleware(
@@ -12,7 +20,6 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 
 def analyze_texte(texte :str):
     mot_cle=nltk.word_tokenize(texte)
@@ -24,13 +31,86 @@ def generer_reponse(texte: str):
 def formater_reponse(texte: str):
     return {"reponse_formater":"reponse vide formater"}
 
-
-
 class AnalyseTexteInput(BaseModel):
     texte: str
+
 @app.post("/analyse")
 def analyse_endpoint(analyse_input: AnalyseTexteInput):
+    #miniscule
+    texte=(analyse_input.texte).lower()
+    words=nltk.word_tokenize(texte)
+    print(words)
+    #stopwords
+    stop_words = set(stopwords.words('english'))
+    tokens = [word for word in words if word not in stop_words]
+    print(tokens) 
+    #liste des ponctuation
+    punctuation = set ('!"#$%&\'()*+,-./:;<=>?@[\\]^_{|}')
+    #suppression des ponctuations
+    tokens = [word for word in tokens if word.lower() not in punctuation]
+    lemmatizer = WordNetLemmatizer()
+    lemmatized_words = [lemmatizer.lemmatize(word) for word in tokens]
+    print(lemmatized_words)
+
+    query = " ".join(lemmatized_words) + " In context of computer Science"
+    print(query)
+    #on doit utiliser OpenAI
+    reponse=query_openai(query)
+    return {"msg": reponse}
+ 
+
+@app.post("/query_openai")
+def query_openai(query: str):
+    api_key = "the key"
+    openai.api_key = api_key  
+
+    Client = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "You are a computer science university teacher"},
+            {"role": "assistant", "content": "You are specialized in AI, machine learning, and deep learning"},
+            {"role": "user", "content": query}
+        ]
+    )
+
+    #return {"msg": Client.choices[0].message["content"]}
+    response = Client.choices[0].message["content"]
+    #print(response)
+    return response
+
+@app.post("/analyseV1")
+def analyse_endpointv1(analyse_input: AnalyseTexteInput):
     print(analyse_input)
+    #miniscule
+    texte=(analyse_input.texte).lower()
+    #ponctuation
+    texte = ' '.join([char for char in texte if char not in string.punctuation])
+    #texte.translate(str.maketrans("", "", string.punctuation))
+
+    #erreur:Faute d'orthographe
+    """blob = TextBlob(texte)
+    texte = blob.correct()
+    print(texte.words)
+    print(type(texte.words))"""
+
+    #tokenisation
+    tokens=nltk.word_tokenize(texte)
+    print(tokens)
+
+    #stopwords
+    stop_words = set(stopwords.words('english'))
+    tokens = [word for word in tokens if word not in stop_words]
+    print(tokens)
+
+    #Stemmer & Lemmatization
+    
+    #porter = PorterStemmer()
+    
+    lemmatizer = WordNetLemmatizer()
+    #stemmed_words = [porter.stem(word) for word in tokens]
+    lemmatized_words = [lemmatizer.lemmatize(word) for word in tokens]
+    print(lemmatized_words)
+
     return {"msg": analyse_input}
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)
--- ChatbotPI_IHEB/FRONTEND/js/app.js
+++ ChatbotPI_IHEB/FRONTEND/js/app.js
@@ -1,12 +1,22 @@
-var BTN=document.querySelector("button")
-var TEXTAREA=document.querySelector("textarea")
+var BTN=document.querySelector("#benv")
+var TEXTAREA=document.querySelector("#textSpeech")
 var DIV=document.querySelector("#reponse_msg")
+var BTN_MIC=document.querySelector("#bMic") 
+var BTN_SP=document.querySelector("#bSpeak")
+//var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition
+var recognition = new webkitSpeechRecognition();
+recognition.continuous = false;
+recognition.lang = 'en-US';
+recognition.interimResults = false;
+recognition.maxAlternatives = 1;
 
 //EVENEMENT
 BTN.addEventListener("click", chatBot)
+BTN_MIC.addEventListener("click", speechToText)
 
 //fonction principale
 function chatBot(){
+
     let text=TEXTAREA.value
     //je dois communiquer avec le backend
     var url_backend="http://127.0.0.1:8000/analyse"
@@ -21,7 +31,21 @@ function chatBot(){
     .then(reponse=>{
         reponse.json()
         .then(data=>{
-            console.log(data)
+            BTN_SP.style.display=""
+            BTN_SP.addEventListener("click",texteToSpeech(data.msg))
+            console.log(data.msg)
+            var i = 0;
+            var txt = data.msg; /* The text */
+            var speed = 100; /* The speed/duration of the effect in milliseconds */
+
+            function typeWriter() {
+            if (i < txt.length) {
+                DIV.innerHTML += txt.charAt(i);
+                i++;
+                setTimeout(typeWriter, speed);
+            }
+            }
+            typeWriter()
         })
     })
     .catch(e=>{
@@ -30,3 +54,27 @@ function chatBot(){
 
 
 }
+
+function texteToSpeech(texte){
+    //lire sous format audio
+    let utterance = new SpeechSynthesisUtterance(texte);
+    speechSynthesis.speak(utterance);
+}
+
+function speechToText(){
+    alert("Je suis speech to text")
+    //1ère partie déclencher l'API Speech To Text
+    recognition.start();
+
+}
+
+recognition.onresult = function(event) {
+
+    //2ème partie récupérer le texte
+    var message = event.results[0][0].transcript;
+    console.log('Result received: ' + message + '.');
+     console.log('Confidence: ' + event.results[0][0].confidence);
+
+    //3ème partie remplir l'input en utilisant ce texte
+    TEXTAREA.value=message
+  }
